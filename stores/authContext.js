fc5afd26e421d6cbc4f407ac05906e0171aaa69f
baymax44 <smartbay2021@outlook.com>
2020-04-16 01:57:28 -0400

    created auth context

---
+++ stores/authContext.js
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
+
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false
+})
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    // init netlify identity connection
+    netlifyIdentity.init()
+  }, [])
+
+  return (
+    <AuthContext.Provider
+      value={user}
+    >
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
