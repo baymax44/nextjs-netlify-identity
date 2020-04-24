1de01856aea899f8bc86a00cac21d37a0dd2e244
baymax44 <smartbay2021@outlook.com>
2020-04-24 00:51:03 -0400

    netlify functions with identity

---
+++ functions/guides.js
@@ -0,0 +1,20 @@
+exports.handler = async (event, context) => {
+  const guides = [
+    { title: 'Beat all Zelda Bosses Like a Boss', author: 'mario' },
+    { title: 'Mario Kart Shortcuts You Never Knew Existed', author: 'luigi' },
+    { title: 'Ultimate Street Fighter Guide', author: 'chun-li' },
+  ]
+
+  if (context.clientContext.user) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(guides)
+    }
+  }
+
+  return {
+    statusCode: 401,
+    body: JSON.stringify({ msg: 'Ah, ah, ah you must be logged in to see this' })
+  }
+
+}
--- pages/guides.js
+++ pages/guides.js
@@ -1,12 +1,21 @@
-import { useEffect } from 'react'
+import { useContext, useEffect } from 'react'
+import AuthContext from '../stores/authContext'
 import styles from '../styles/Guides.module.css'
 
 export default function Guides() {
+  const { user, authReady } = useContext(AuthContext)
+
   useEffect(() => {
-    fetch('/.netlify/functions/supermario')
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }, [])
+    if (authReady) {
+      fetch('/.netlify/functions/guides', user && {
+        headers: {
+          Authorization: 'Bearer ' + user.token.access_token
+        }
+      })
+      .then(res => res.json())
+      .then(data => console.log(data))
+    }
+  }, [user])
 
   return (
     <div className={styles.guides}>
