d2e86b984dce7e0985df8a137efb667f3b42aa2c
baymax44 <smartbay2021@outlook.com>
2020-04-27 00:59:53 -0400

    content guard on the front-end side

--- pages/guides.js
+++ pages/guides.js
@@ -4,6 +4,8 @@ import styles from '../styles/Guides.module.css'
 
 export default function Guides() {
   const { user, authReady } = useContext(AuthContext)
+  const [guides, setGuides] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     if (authReady) {
@@ -12,14 +14,38 @@ export default function Guides() {
           Authorization: 'Bearer ' + user.token.access_token
         }
       })
-      .then(res => res.json())
-      .then(data => console.log(data))
+      .then(res => {
+        if (!res.ok) {
+          throw Error('You must be logged in to view this content')
+        }
+        return res.json()
+      })
+      .then(data => {
+        setGuides(data)
+        setError(null)
+      })
+      .catch(err => {
+        setError(err.message)
+        setGuides(null)
+      })
     }
   }, [user])
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      {!authReady && <div>Loading...</div>}
+
+      {error && <div className={styles.error}>
+        <p>{ error }</p>
+      </div>}
+
+      {guides && guides.map(guide => {
+        <div key={guide.title} className={styles.card}>
+          <h3>{ guide.title }</h3>
+          <h4>Written by { guide.author }</h4>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula</p>
+        </div>
+      })}        
     </div> 
   )
 }
